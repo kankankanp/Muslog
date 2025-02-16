8f538c86690ad9f4ae14a015f5859c4df37d5e64
kankankanp <minamikan0825@gmail.com>
2025-02-16 15:17:57 +0900

    fix-registration

--- src/app/lib/auth/middleware.ts
+++ src/app/lib/auth/middleware.ts
@@ -1,7 +1,18 @@
-import NextAuth from "next-auth";
-import { authConfig } from "../../../../auth.config";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { auth } from "./auth";
 
-export default NextAuth(authConfig).auth;
+export async function middleware(req: NextRequest) {
+  const session = await auth();
+
+  if (req.nextUrl.pathname.startsWith("/dashboard")) {
+    if (!session?.user) {
+      return NextResponse.redirect(new URL("/registration/login", req.url));
+    }
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
