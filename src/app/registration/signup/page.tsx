6116c8b7b1d0a0f5fc418b4e2a9698904036a010
kankankanp <minamikan0825@gmail.com>
2025-02-28 17:43:41 +0900

    fix-builderror

--- src/app/registration/signup/page.tsx
+++ src/app/registration/signup/page.tsx
@@ -1,4 +1,4 @@
-import SignupForm from "../../components/elements/SignupForm";
+import SignupForm from "@/app/components/elements/SignupForm";
 
 export default function Signup() {
   return <SignupForm />;
