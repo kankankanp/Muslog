d70f05a8c9c3185b5532e4ea1fbb32118eb8e230
kankankanp <minamikan0825@gmail.com>
2025-03-03 20:34:24 +0900

    tweak

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -3,14 +3,14 @@ import prisma from "@/app/lib/db/prisma";
 import { NextResponse } from "next/server";
 
 //ブログ全記事取得API
-export const GET = async (req: Request) => {
-  try {
-    const posts = await prisma.post.findMany();
-    return NextResponse.json({ message: "Success", posts }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ message: "Error", error }, { status: 500 });
-  }
-};
+// export const GET = async (req: Request) => {
+//   try {
+//     const posts = await prisma.post.findMany();
+//     return NextResponse.json({ message: "Success", posts }, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json({ message: "Error", error }, { status: 500 });
+//   }
+// };
 
 //ブログ投稿用API
 export const POST = async (req: Request) => {
