85b12008b4774b0e3f187772943a008cbbf33def
kankankanp <minamikan0825@gmail.com>
2025-03-05 01:17:21 +0900

    [WIP]create-bookflip-logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import Header from "./components/layouts/Header";
 import Footer from "./components/layouts/Footer";
+import DemoBook from "./components/elements/Book";
+import { fetchAllBlogs } from "./lib/utils/blog";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await fetchAllBlogs();
+
   return (
     <>
       <Header />
       <main className="dark:bg-gray-900 bg-gray-100 min-h-screen flex items-center justify-center">
-        <div className="bg-white p-10 rounded-lg shadow-lg text-center dark:bg-gray-800">
-          <h3 className="text-2xl font-bold dark:text-white text-gray-900">
-            Welcome to My App!
-          </h3>
-        </div>
+        <DemoBook posts={posts} />
       </main>
       <Footer />
     </>
