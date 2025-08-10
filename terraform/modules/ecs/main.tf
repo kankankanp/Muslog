222264ff72c2cf6ed7a53b5cde81ecfecfb60489
kankankanp <minamikan0825@gmail.com>
2025-08-10 13:15:24 +0900

    fix:タスク実行ロールの付与

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -70,7 +70,7 @@ resource "aws_iam_policy" "ecs_secrets_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_secrets_policy_attachment" {
-  role       = aws_iam_role.ecs_task_role.name
+  role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = aws_iam_policy.ecs_secrets_policy.arn
 }
 
