67b45f38eb18ac6f93f05e76a29349a6932ea2ab
kankankanp <minamikan0825@gmail.com>
2025-08-28 23:59:23 +0900

    fix:コメント消す

--- backend/internal/handler/websocket_handler.go
+++ backend/internal/handler/websocket_handler.go
@@ -19,10 +19,8 @@ const (
 	// Time allowed to read the next pong message from the peer.
 	pongWait = 60 * time.Second
 
-	// Send pings to peer with this period. Must be less than pongWait.
 	pingPeriod = (pongWait * 9) / 10
 
-	// Maximum message size allowed from peer.
 	maxMessageSize = 512
 )
 
